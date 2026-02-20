7f66462b470061e2d5e3a44e1f6255f15193c8c3
abhinavatgit24 <asabhinav2005@gmail.com>
2026-02-20 23:56:35 +0530

    Time: 239 ms (14.26%), Space: 54.4 MB (27.43%) - LeetHub

---
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+
+        List<int[]> events = new ArrayList<>();
+
+        for (int[] b : buildings) {
+            events.add(new int[]{b[0], -b[2]});
+            events.add(new int[]{b[1], b[2]});
+        }
+
+        Collections.sort(events, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        heap.add(0);
+
+        int prevMax = 0;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int[] e : events) {
+
+            int x = e[0];
+            int h = e[1];
+
+            if (h < 0) {
+                heap.add(-h);
+            } else {
+                heap.remove(h);
+            }
+
+            int currMax = heap.peek();
+
+            if (currMax != prevMax) {
+                result.add(Arrays.asList(x, currMax));
+                prevMax = currMax;
+            }
+        }
+
+        return result;
+    }
+}
