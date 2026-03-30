b7eab60fe3a5d73325ace88dee65a39a9f4ee4e8
abhinavatgit24 <asabhinav2005@gmail.com>
2026-03-30 23:58:31 +0530

    Time: 26 ms (5.12%), Space: 43.6 MB (90.58%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (mapST.containsKey(a) && mapST.get(a) != b) return false;
+            if (mapTS.containsKey(b) && mapTS.get(b) != a) return false;
+
+            mapST.put(a, b);
+            mapTS.put(b, a);
+        }
+
+        return true;
+    }
+}
